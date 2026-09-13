c5c4a26e65df46f9d6d973d2753e1b36860ce499
kerator221 <ggmail04@mail.ru>
2026-09-13 18:01:16 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+    description = "nixos dotfiles";
+    
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05"; 
+        
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, home-manager, ...}: {
+        nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./configuration.nix
+                home-manager.nixosModules.home-manager
+                {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users."ghosty" = import ./home.nix;
+                }
+            ];
+        };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,1 @@
+/home/ghosty/.config/home-manager/home.nix
