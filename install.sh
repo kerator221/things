0aa857b3abb636995da19b1c4ff264df1378303d
kerator221 <ggmail04@mail.ru>
2026-09-16 00:21:37 +0800

    secret generation + auto nixos-rebuild

--- install.sh
+++ install.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
-set -e 
+set -e
 
-#folders
-#sudo mkdir -p /etc/nixos
-#mkdir -p ~/.config/home-manager
-mkdir -p ~/.config/kitty
+# folders
 mkdir -p ~/.config/hypr
+mkdir -p ~/.config/kitty
 mkdir -p ~/images/wallpapers
 
-# main files
-#sudo ln -sf ~/nixos-dots/configuration.nix /etc/nixos/configuration.nix
-#ln -sf ~/nixos-dots/home.nix ~/.config/home-manager/home.nix
-
-#wallpapers are hardcoded now maybe later i need to rewrite them 
+# wallpapers are hardcoded now maybe later i need to rewrite them 
 ln -sf ~/nixos-dots/wallpaper.png ~/images/wallpapers/wallpaper.png
 
 # kitty
 ln -sf ~/nixos-dots/kitty.conf ~/.config/kitty/kitty.conf
 
-#hypr
+# hypr
 ln -sf ~/nixos-dots/hypr/animations.lua ~/.config/hypr/animations.lua
 ln -sf ~/nixos-dots/hypr/custom.lua ~/.config/hypr/custom.lua
 ln -sf ~/nixos-dots/hypr/decorations.lua ~/.config/hypr/decorations.lua
@@ -30,6 +24,12 @@ ln -sf ~/nixos-dots/hypr/monitors.lua ~/.config/hypr/monitors.lua
 ln -sf ~/nixos-dots/hypr/permissions.lua ~/.config/hypr/permissions.lua
 ln -sf ~/nixos-dots/hypr/windows.lua ~/.config/hypr/windows.lua
 
-#apply things
-#sudo nixos-rebuild switch
-#home-manager switch
+# tg-ws-proxy secret
+secret=$(openssl rand -hex 16)
+mkdir -p ~/.config/tg-ws-proxy
+echo "$secret" > ~/.config/tg-ws-proxy/secret.txt
+
+# hardware
+#sudo nixos-generate-config --dir "$HOME/nixos-dots" idk lol i think ure already have a hardware config if u starting it from working system
+sudo nixos-rebuild switch --flake path:.#nixos
+
