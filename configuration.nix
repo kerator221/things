3254354ed02204e21f765048dc49dab28ea181d5
kerator221 <ggmail04@mail.ru>
2026-09-17 00:28:20 +0800

    123

--- configuration.nix
+++ configuration.nix
@@ -91,7 +91,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.${username} = {
      isNormalUser = true;
-     extraGroups = [ "camera" "wheel" ]; # Enable ‘sudo’ for the user.
+     extraGroups = [ "camera" "wheel" "libvirt" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
        tree
      ];
