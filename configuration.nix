2bb2c6687dabdd63d0a316f68cb005b844a0f191
kerator221 <ggmail04@mail.ru>
2026-09-17 01:47:56 +0800

    noto-fonts-extra

--- configuration.nix
+++ configuration.nix
@@ -171,6 +171,7 @@
       nerd-fonts.jetbrains-mono
       nerd-fonts.fira-code
       nerd-fonts.iosevka
+      noto-fonts-extra
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
