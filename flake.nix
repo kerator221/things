964a2a2eafc7804ab23723847f93e3ca86791f6c
kerator221 <ggmail04@mail.ru>
2026-09-17 00:16:39 +0800

    init

--- flake.nix
+++ flake.nix
@@ -19,8 +19,8 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.${username} = { pkgs, ... }: import ./home.nix {
-            inherit pkgs username;
+          home-manager.users.${username} = { config, pkgs, ... }: import ./home.nix {
+            inherit config pkgs username;
           };
         }
       ];
