7253bbf406e8d0e0702b154a106d633d090dc387
kerator221 <ggmail04@mail.ru>
2026-09-18 19:03:33 +0800

    added unrar

--- configuration.nix
+++ configuration.nix
@@ -130,6 +130,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
    environment.systemPackages = with pkgs; [
+      unrar
       unzip
       zip
       curl
