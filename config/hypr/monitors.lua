652e3b77540a1870b8d2fb01588bf8f90e295a7c
kerator221 <ggmail04@mail.ru>
2026-09-24 16:29:31 +0800

    added fallback for other monitors

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -3,6 +3,7 @@
 ------------------
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+--My setup
 hl.monitor({
     output   = "HDMI-A-1",
     mode     = "1920x1080@100",
@@ -15,4 +16,12 @@ hl.monitor({
    mode      = "1600x900@60",
    position  = "0x0",
    scale     = "auto",
+})
+
+-- For others
+hl.monitor({
+    output   = "",
+    mode     = "preferred",
+    position = "auto",
+    scale    = "auto",
 })
