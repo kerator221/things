1d400e87a3599321b4be479d6a50d3dabd9b894d
kerator221 <ggmail04@mail.ru>
2026-09-25 22:08:04 +0800

    auto adjust to theme color

--- config/hypr/decorations.lua
+++ config/hypr/decorations.lua
@@ -2,17 +2,19 @@
 ---- LOOK AND FEEL ----
 -----------------------
 
+local c = require("colors")
+
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 2,
-        gaps_out = 36,
+        gaps_in  = 4,
+        gaps_out = 8,
 
-        border_size = 1,
+        border_size = 2,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-            inactive_border = "rgba(595959aa)",
+            active_border   = { colors = {c.primary_container, c.on_primary_fixed_variant}, angle = 45 },
+            inactive_border = c.surface_bright,
         },
 
         -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
@@ -25,18 +27,18 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 6,
+        rounding       = 16,
         rounding_power = 2,
 
         -- Change transparency of focused and unfocused windows
-        active_opacity   = 0.99,
+        active_opacity   = 0.995,
         inactive_opacity = 0.9,
 
         shadow = {
-            enabled      = true,
+            enabled      = false,
             range        = 4,
-            render_power = 3,
-            color        = 0xee1a1a1a,
+            render_power = 2,
+            color        = c.scrim,
         },
 
         blur = {
